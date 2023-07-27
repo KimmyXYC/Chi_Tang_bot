a03b5c3bf1a6546ea0c56564e8a1090741859454
エルフです <kimmyxyc@163.com>
2023-07-27 20:52:00 +0800

    init

--- App/Event.py
+++ App/Event.py
@@ -8,12 +8,15 @@ from telebot.types import InlineQueryResultArticle, InputTextMessageContent
 
 
 async def chi_tang(bot, query, fadian, name="池塘"):
-    fadian_text = random.choice(fadian).format(name=name)
-    result = InlineQueryResultArticle(
-        id='1',
-        title='嘿嘿嘿~~~立即发癫',
-        input_message_content=InputTextMessageContent(
-            message_text=fadian_text
-        )
-    )
-    await bot.answer_inline_query(query.id, [result], cache_time=0)
+    fadian_texts = random.sample(fadian, 3)
+    result = []
+    for id_number in range(3):
+        result.append(InlineQueryResultArticle(
+            id=id_number+1,
+            title='嘿嘿嘿~~~立即发癫',
+            description=f"{fadian_texts[id_number].format(name=name)[:30]}...",
+            input_message_content=InputTextMessageContent(
+                message_text=fadian_texts[id_number].format(name=name)
+            )
+        ))
+    await bot.answer_inline_query(query.id, result, cache_time=0)
